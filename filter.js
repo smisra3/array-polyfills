b283dbf43986ac1d885638d312e3efd95136b9f2
SAPIENT\smis64 <smisra3@sapient.com>
2018-06-15 10:52:32 +0530

    Adding the __filter polyfill for Array.prototype.filter.

---
+++ filter.js
@@ -0,0 +1,21 @@
+/**
+ *This method is a polyfill for Array.prototype.filter which returns the same array when the predicate function is not 
+  passed or when the array lenght is zero, othrwise a filtered array is returned according to the predicate. It does
+  not modify the original array.
+  The order in which the params to the predicate function is to be passed is as follows: (index, element, array), where
+  index is current index of the element being iterated and array is the array on which the __filter is called.  
+ * @param {Function} fn Is the predicate function used to check for the condition to filter.
+ * @returns {Array} An empty array or the new filtered array. 
+ */
+
+Array.prototype.__filter = function (fn) {
+  if (!fn || !this.length)
+    return this;
+  var newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    if (fn(i, this[i], this)) {
+      newArray.push(this[i]);
+    }
+  }
+  return newArray;
+}
