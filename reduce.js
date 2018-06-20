8ae2fdcd20c12586b9ca7c68960a37464b533e19
SAPIENT\smis64 <smisra3@sapient.com>
2018-06-20 14:49:42 +0530

    Saving work

---
+++ reduce.js
@@ -0,0 +1,25 @@
+/**
+ *This method is a polyfill for Array.prototype.reduce which returns the same array when the predicate function is not 
+  passed or when the array lenght is zero, othrwise a filtered array is returned according to the predicate. It does
+  not modify the original array.
+  The order in which the params to the predicate function is to be passed is as follows: (index, element, array), where
+  index is current index of the element being iterated and array is the array on which the __filter is called.  
+ * @param {Function} fn Is the predicate function used to check for the condition to filter.
+ * @param {Object} thisArg Is the optional this argument passed to call the predicate function with a different this value.
+ * @returns {Array} An empty array or the new filtered array. 
+ */
+Array.prototype.__reduce = function (fn, accumulator) {
+    let startIndex;
+    if (!fn || typeof fn !== 'function' || !this.length)
+        throw new TypeError();
+    return this.length === 1 && !accumulator ? this[0] : (accumulator && !this.length ? accumulator : false)
+    !accumulator ? (accumulator = this[0], startIndex = 1) : startIndex = 0;
+    for (var i = startIndex; i < this.length; i++) {
+        accumulator = fn(accumulator, this[i], i, this)
+    }
+    return accumulator;
+}
+
+var a = [1, 2, 3, 4, 5];
+var sum = a.__reduce((accumulator, currentValue) => accumulator * currentValue, 1)
+console.log(sum);
